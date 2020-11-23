3ccb91cdb7e0b634db337aa532baf0e8aaca578f
Jonas Huhndorf <jonas.huhndorf@gmail.com>
2020-11-23 21:19:04 +0100

    Fixes in next_move_survival_calculation

--- globale_functions/move_calculations.py
+++ globale_functions/move_calculations.py
@@ -1,7 +1,15 @@
 from api.action_play import amount_of_moves
 
 
-def next_move_survival_calculation(x_position, y_position, speed, direction, field, x_field_size, y_field_size):
+def next_move_survival_calculation(
+        x_position,
+        y_position,
+        speed,
+        direction,
+        field,
+        x_field_size,
+        y_field_size
+):
     x, y = 0, 0
     if direction == 'up':
         y = 1
@@ -13,10 +21,11 @@ def next_move_survival_calculation(x_position, y_position, speed, direction, fie
         x = 1
 
     for n in range(1, speed):
-        if amount_of_moves != 6 or n != 1 or n != speed:
+        if amount_of_moves != 6 or n == 1 or n == speed:
             x_location = x_position + x * n
             y_location = y_position + y * n
             if 0 < x_location < x_field_size and 0 < y_location < y_field_size:
-                if field[x_location][y_location] != 0:
-                    return False
+                return False
+            elif field[x_location][y_location] != 0:
+                return False
     return True
