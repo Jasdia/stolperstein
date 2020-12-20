5dffdeefeba4dafbe66acd8385efeb0a04a7c6a5
Jonas Huhndorf <jonas.huhndorf@gmail.com>
2020-12-20 16:46:01 +0100

    Comments and deprecation in dataclasses for the neural network.

--- data_classes/neural_network/SimpleGame.py
+++ data_classes/neural_network/SimpleGame.py
@@ -1,9 +1,16 @@
+# TODO("Remove 'created with'-comment.")
 # created with: https://app.quicktype.io/
 
+# Python-libraries
+from deprecated import deprecated
 from dataclasses import dataclass
+
+# Other modules from this project
+# classes:
 from data_classes.neural_network.SimplePlayer import SimplePlayer
 
 
+@deprecated(reason="Probably oversize!")
 @dataclass
 class SimpleGame:
     width: int
--- data_classes/neural_network/SimplePlayer.py
+++ data_classes/neural_network/SimplePlayer.py
@@ -1,8 +1,12 @@
+# TODO("Remove 'created with'-comment.")
 # created with: https://app.quicktype.io/
 
+# Python-libraries
+from deprecated import deprecated
 from dataclasses import dataclass
 
 
+@deprecated(reason="Probably oversize!")
 @dataclass
 class SimplePlayer:
     x: int
