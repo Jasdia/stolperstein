72e80eeb8940fdc5cef23f29cd79af18a1c62017
Jonas Huhndorf <jonas.huhndorf@gmail.com>
2020-12-25 17:52:58 +0100

    Removed useless print-statement.

--- calculate_next_step/calculation.py
+++ calculate_next_step/calculation.py
@@ -21,7 +21,6 @@ def start_calculation(test_depth, step, play_map):
     mc_globals.rest_highest_test_step()
     play_map = simplify_game_data(play_map)
     for i in range(test_depth):
-        print(i)
         start_new_thread(_move_iteration, (i, step, play_map, ))
 
 
