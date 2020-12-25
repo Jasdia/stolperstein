380f610a770bd0594d06d7abba7b1110f7bf6d0f
Jonas Huhndorf <jonas.huhndorf@gmail.com>
2020-12-25 11:30:14 +0100

    New Test for _test_all_options().

--- testing/calculate_next_step/calculation/test_calculation.py
+++ testing/calculate_next_step/calculation/test_calculation.py
@@ -3,7 +3,7 @@ import os
 from json import loads
 from pathlib import Path
 
-from calculate_next_step.calculation import _set_move
+from calculate_next_step.calculation import _set_move, _test_all_options
 from data_classes.manual_calculation.ManuelCalculatedGame import ManuelCalculatedGame
 from data_classes.manual_calculation.ManuelCalculatedPlayer import ManuelCalculatedPlayer
 import api.api_feedback_global_variables as api_globals
@@ -14,31 +14,55 @@ class TestCalculation(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(TestCalculation, self).__init__(*args, **kwargs)
         self._root_path = str(Path(__file__).parent.absolute())
-        self.path = self._root_path + "/_set_move"
         api_globals._init()
 
     def test__set_move(self):
-        files = next(os.walk(self.path))[2]
+        path = self._root_path + "/_set_move"
+        files = next(os.walk(path))[2]
         count = len(files)
-        print()
-        for i in range(int(count/3)):
-            tmp_file = open(self.path + "/" + str(i) + "_data.json", "r")
+        for i in range(int(count / 3)):
+            tmp_file = open(path + "/" + str(i) + "_data.json", "r")
             test_data = loads(tmp_file.read())
             tmp_file.close()
             test_data["players"] = [ManuelCalculatedPlayer(**player) for player in test_data["players"].values()]
             test_data_class = ManuelCalculatedGame(**test_data)
 
-            tmp_file = open(self.path + "/" + str(i) + "_parameters.json", "r")
+            tmp_file = open(path + "/" + str(i) + "_parameters.json", "r")
             parameters = loads(tmp_file.read())
             tmp_file.close()
 
             api_globals.amount_of_moves = parameters["api_globals.amount_of_moves"]
 
-            tmp_file = open(self.path + "/" + str(i) + "_result.json", "r")
+            tmp_file = open(path + "/" + str(i) + "_result.json", "r")
             result_data = loads(tmp_file.read())
             tmp_file.close()
             result_data["players"] = [ManuelCalculatedPlayer(**player) for player in result_data["players"].values()]
             result_data_class = ManuelCalculatedGame(**result_data)
 
             test_result = _set_move(parameters["position"], parameters["action"], test_data_class)
-            self.assertEqual(result_data_class, test_result, msg="_set_move-test number: " + str(i) + " failed.")
+            self.assertEqual(result_data_class, test_result, msg="_set_move number: " + str(i) + " failed.")
+
+    def test__test_all_options(self):
+        path = self._root_path + "/_test_all_options"
+        files = next(os.walk(path))[2]
+        count = len(files)
+        for i in range(int(count / 3)):
+            tmp_file = open(path + "/" + str(i) + "_data.json", "r")
+            test_data = loads(tmp_file.read())
+            tmp_file.close()
+            test_data["players"] = [ManuelCalculatedPlayer(**player) for player in test_data["players"].values()]
+            test_data_class = ManuelCalculatedGame(**test_data)
+
+            tmp_file = open(path + "/" + str(i) + "_parameters.json", "r")
+            parameters = loads(tmp_file.read())
+            tmp_file.close()
+
+            api_globals.amount_of_moves = parameters["api_globals.amount_of_moves"]
+
+            tmp_file = open(path + "/" + str(i) + "_result.json", "r")
+            result_data = loads(tmp_file.read())
+            tmp_file.close()
+            result_data = [result_data["death_count"], result_data["killed_count"]]
+
+            test_result = _test_all_options(parameters["position"], parameters["death_count"], parameters["killed_count"], test_data_class, parameters["test_depth"], parameters["tested_move"])
+            self.assertEqual(result_data, test_result, msg="_test_all_options number: " + str(i) + " failed.")
