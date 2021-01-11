86b4ed4da18d972a2e1b27921e9ade37726e52c4
Jonas Huhndorf <jonas.huhndorf@gmail.com>
2021-01-11 10:40:13 +0100

    Reduce amount_of_players for calculation.

--- calculate_next_step/data_simplification.py
+++ calculate_next_step/data_simplification.py
@@ -6,14 +6,20 @@ from data_classes.ManuelCalculatedGame import ManuelCalculatedGame
 
 # Simplifies the data and maps Game on ManuelCalculatedGames
 def simplify_game_data(play_map):
-    player_list = []
+    tmp_player_list = []
     for player in play_map['players'].items():
         if player[1]['active']:
             simple_player = _simple_player_mapping(player[1], int(player[0]))
             if not player[0] == str(play_map['you']):
-                player_list.append(simple_player)
+                tmp_player_list.append(simple_player)
             else:
-                player_list.insert(0, simple_player)
+                tmp_player_list.insert(0, simple_player)
+
+    player_list = [tmp_player_list[0]]
+    for i in range(1, len(tmp_player_list)):
+        if not (abs(tmp_player_list[0].x - tmp_player_list[i].x) > tmp_player_list[0].speed + tmp_player_list[i].speed+2
+                or abs(tmp_player_list[0].y - tmp_player_list[i].y) > tmp_player_list[0].speed + tmp_player_list[i].speed):
+            player_list.append(tmp_player_list[i])
 
     for column in range(play_map['height']):
         for row in range(play_map['width']):
