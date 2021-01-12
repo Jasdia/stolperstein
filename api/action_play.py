24b5f1b3de5e0e843ae22166ebed180fb1ae1f47
Jonas Huhndorf <jonas.huhndorf@gmail.com>
2021-01-12 19:33:46 +0100

    Renaming alias of globals

--- api/action_play.py
+++ api/action_play.py
@@ -13,7 +13,7 @@ from ctypes import c_int
 from api.json_answer import generated_json
 from calculate_next_step.simple_calculation import move_iteration
 # global variables:
-import global_variables as globals
+import global_variables as internal_globals
 
 
 # Connection and communication with the server.
@@ -43,13 +43,13 @@ async def start_ws():
                         info("We are still alive!")
                         with amount_of_moves.get_lock() and action.get_lock():
                             Process(target=move_iteration, args=(amount_of_moves.value, play_map, action,
-                                                                 amount_of_moves, globals.move_list)).start()
+                                                                 amount_of_moves, internal_globals.move_list)).start()
 
                         # Set sleep-time before answering.
                         deadline = datetime.strptime(play_map['deadline'], '%Y-%m-%dT%H:%M:%SZ')
                         sleep_time = (deadline - datetime.utcnow()).total_seconds()
                         # One second for answering.
-                        sleep_time -= globals.answer_time_for_the_bot
+                        sleep_time -= internal_globals.answer_time_for_the_bot
 
                         # Just waits if the deadline is in the future.
                         # It could - for example - be the case, that the server sends an old json-file.
@@ -57,12 +57,12 @@ async def start_ws():
                             sleep(sleep_time)
 
                         # Retrying to send the answer to the server.
-                        for _ in range(globals.amount_of_retrying_sending_an_answer):
+                        for _ in range(internal_globals.amount_of_retrying_sending_an_answer):
                             try:
                                 # Example of sending an answer for the server.
                                 with action.get_lock():
-                                    await websocket.send(generated_json(f'{globals.move_list[action.value]}'))
-                                    info("answer sent: " + globals.move_list[action.value])
+                                    await websocket.send(generated_json(f'{internal_globals.move_list[action.value]}'))
+                                    info("answer sent: " + internal_globals.move_list[action.value])
 
                                 with amount_of_moves.get_lock():
                                     amount_of_moves.value += 1
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # InformatiCup 2021 - spe_ed bot
 # Team:
-# ███████╗████████╗ ██████╗ ██╗     ██████╗ ███████╗██████╗ ███████╗████████╗███████╗██╗███╗   ██╗███████╗
-# ██╔════╝╚══██╔══╝██╔═══██╗██║     ██╔══██╗██╔════╝██╔══██╗██╔════╝╚══██╔══╝██╔════╝██║████╗  ██║██╔════╝
-# ███████╗   ██║   ██║   ██║██║     ██████╔╝█████╗  ██████╔╝███████╗   ██║   █████╗  ██║██╔██╗ ██║█████╗
-# ╚════██║   ██║   ██║   ██║██║     ██╔═══╝ ██╔══╝  ██╔══██╗╚════██║   ██║   ██╔══╝  ██║██║╚██╗██║██╔══╝
-# ███████║   ██║   ╚██████╔╝███████╗██║     ███████╗██║  ██║███████║   ██║   ███████╗██║██║ ╚████║███████╗
-# ╚══════╝   ╚═╝    ╚═════╝ ╚══════╝╚═╝     ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝   ╚══════╝╚═╝╚═╝  ╚═══╝╚══════╝
+# ███████╗████████╗ ██████╗ ██╗     ██████╗ ███████╗██████╗ ███████╗████████╗███████╗██╗███╗   ██╗
+# ██╔════╝╚══██╔══╝██╔═══██╗██║     ██╔══██╗██╔════╝██╔══██╗██╔════╝╚══██╔══╝██╔════╝██║████╗  ██║
+# ███████╗   ██║   ██║   ██║██║     ██████╔╝█████╗  ██████╔╝███████╗   ██║   █████╗  ██║██╔██╗ ██║
+# ╚════██║   ██║   ██║   ██║██║     ██╔═══╝ ██╔══╝  ██╔══██╗╚════██║   ██║   ██╔══╝  ██║██║╚██╗██║
+# ███████║   ██║   ╚██████╔╝███████╗██║     ███████╗██║  ██║███████║   ██║   ███████╗██║██║ ╚████║
+# ╚══════╝   ╚═╝    ╚═════╝ ╚══════╝╚═╝     ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝   ╚══════╝╚═╝╚═╝  ╚═══╝
 # programmed by Sandra Jasmine Bernich and Jonas Marvin Huhndorf
 
 # ASCII-Art designed with: http://patorjk.com/software/taag/
@@ -18,14 +18,11 @@ from logging import info, INFO, basicConfig, root
 # Other modules from this project
 # functions:
 from api.action_play import start_ws
-# global variables (see conventions in *_global_variables.py):
-import calculate_next_step.mc_global_variables as mc_globals
-import global_variables as api_globals
+import global_variables as internal_globals
 
 if __name__ == '__main__':
     # Initialize all global-containers
-    api_globals._init()
-    mc_globals._init()
+    internal_globals._init()
 
     # Sets the logging-level to INFO.
     basicConfig()
@@ -34,32 +31,3 @@ if __name__ == '__main__':
     # Start contact to server
     info("The bot has started...")
     get_event_loop().run_until_complete(start_ws())
-
-    # ==================================================================
-    # Just for Testing:
-    # from json import loads
-    # json_testfile = loads(open("./json_test_files/002.json", "r").read())
-    # from calculate_next_step.calculation import start_calculation, _test_all_options_1
-    # from calculate_next_step.data_simplification import simplify_game_data_1
-    # import api.api_feedback_global_variables as api_globals
-    # import calculate_next_step.mc_global_variables as mc_globals
-    # # api_globals.amount_of_moves = 10
-    # # print(json_testfile)
-    # from multiprocessing import Value
-    # from ctypes import c_wchar_p
-    # from timeit import default_timer
-    # # action = Value(c_wchar_p, 'change_nothing')
-    # death_count = Value('i', 0)
-    # killed_count = Value('i', 0)
-    # start_time = default_timer()
-    # _test_all_options_1(0, death_count, killed_count, simplify_game_data_1(json_testfile), 0, True, mc_globals.move_list)
-    # end_time = default_timer()
-    # print(end_time - start_time)
-    # print(death_count.value)
-    # print(killed_count.value)
-    # start_calculation(2, 10, json_testfile, action, 10)
-    # api_globals.game_as_class = map_json_to_dataclass(json_testfile)
-    # create_grid(api_globals.game_as_class.cells, api_globals.game_as_class.players)
-    # start_calculation(1)
-    # from time import sleep
-    # sleep(300)
